e896ca074c52e42eb69cbbdbe7c1963b0c1e0c16
Cube <49463634+RuCu6@users.noreply.github.com>
2023-09-18 22:00:30 +0800

    baidu netdisk

--- Scripts/baidu/baiduCloud.js
+++ Scripts/baidu/baiduCloud.js
@@ -1,29 +1,92 @@
-// 2023-08-07 19:25
+// 2023-09-18 22:00
 
+const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-obj = {
-  product_infos: [
-    {
-      cur_svip_type: "Crack",
-      product_name: "svip2_nd",
-      product_description: "解锁倍速+画质",
-      function_num: 510004015,
-      start_time: 1672502400,
-      buy_description: "无下载加速",
-      buy_time: 980784000,
-      product_id: "问好",
-      auto_upgrade_to_svip: 0,
-      end_time: 4070880000,
-      cluster: "vip",
-      detail_cluster: "svip",
-      status: 0
+if (url.includes("/api/getsyscfg?")) {
+  const switchs = [
+    "active_sigin_text", // 签到文案
+    "ai_search_h5", // ai搜索
+    "album_story_config", // 首页视频故事卡片开关配置
+    "bdnc_commerce_expire_alert_area", // 商业化到期提醒开关
+    "bdnc_commerce_video_ad_area_pad", // 视频贴片广告跳转链接
+    "business_ad_config_area", // 各种插入广告
+    "certification_user_area", // 我的页面 网盘认证
+    "enterprise_banner_area", // 企业运营位
+    "enterprise_bottom_banner", // 企业版banner
+    "enterprise_hot_tools_area", // 企业首页热门工具
+    "enterprise_share_file_list", // 企业空间
+    "enterprise_space_area", // 企业空间
+    "enterprise_space_config_area", // 企业入口
+    "enterprise_space_document_pay_guide", // 开通企业套餐尊享PDF工具
+    "flutter_business_area", // Flutter配置
+    "home_card_area", // 首页卡片
+    "home_recnet_chasing_card_switch", // 首页最近在追tab开关
+    "home_tool_area_all_tool_item_area", // 全部工具角标
+    "ios_carplay_config_area", // 网盘iOS端CarPlay引导功能节点
+    "local_push", // 本地Push
+    "magictrick", // 神机-Sugs区显示灵感库开关配置
+    "magictrick_inspiration_area", // 学霸神器 玩点有趣 社交情感
+    "my_person_service", // 度小满
+    "my_share_tag_area", // 企业权益
+    "new_user_card", // 新人必看
+    "ocr_ai_scan_entrance_area", // 拍一拍home页相机入口飘条
+    "private_background_upload", // 后台传输
+    "public_guide_config", // 引导飘条
+    "public_home_config", // 首页运营
+    "public_imprint_config", // 印迹频道节点配置
+    "push_active_area", // push弹窗
+    "share_Im_idol_area", // idol特权解锁展示
+    "share_tool_area", // 共享页顶部图标
+    "splash_advertise_fetch_config_area", // 开屏广告
+    "splash_advertise_type_area", // 开屏广告
+    "theme_skin_active_area", // 十周年皮肤配置节点
+    "thrid_ad_buads_service", // 穿山甲SDK初始化开关
+    "thrid_ad_funads_service", // 小熊SDK初始化开关
+    "universal_card_area", // 各种卡片
+    "upload_retrieve" // 自动上报配置
+  ];
+  for (let i of switchs) {
+    if (obj?.[i]?.cfg_list?.length > 0) {
+      for (let ii of obj[i].cfg_list) {
+        if (ii?.switch) {
+          ii.switch = "0";
+        }
+        if (ii?.open) {
+          ii.open = "0";
+        }
+      }
     }
-  ],
-  level_info: {
-    current_level: 10
   }
-};
+} else if (url.includes("/membership/user?")) {
+  if (obj?.product_infos) {
+    obj.product_infos = [
+      {
+        cur_svip_type: "Crack",
+        product_name: "svip9_nd",
+        product_description: "超级会员",
+        function_num: 510004015,
+        start_time: 1672502400,
+        buy_description: "",
+        buy_time: 0,
+        product_id: 8,
+        auto_upgrade_to_svip: 0,
+        end_time: 3786883200,
+        cluster: "vip",
+        detail_cluster: "svip",
+        status: 0
+      }
+    ];
+  }
+  if (obj?.level_info) {
+    obj.level_info = {
+      history_value: 1000,
+      current_level: 10,
+      current_value: 1000,
+      history_level: 1
+    };
+  }
+}
 
 $done({ body: JSON.stringify(obj) });
--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,4 +1,4 @@
-// 2023-09-07 12:00
+// 2023-09-18 19:45
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -6,40 +6,6 @@ let body = $response.body;
 
 if (body) {
   switch (true) {
-    // 百度网盘-开屏广告
-    case /^https:\/\/pan\.baidu\.com\/api\/getsyscfg\?/.test(url):
-      try {
-        let obj = JSON.parse(body);
-        if (obj?.splash_advertise_fetch_config_area?.cfg_list) {
-          obj.splash_advertise_fetch_config_area.cfg_list =
-            obj.splash_advertise_fetch_config_area.cfg_list.filter(
-              (i) =>
-                ![
-                  "svip_user_special_advertise_config", // 生日开屏
-                  "autoScroll_timeInterval", // banner广告轮播间隔时间
-                  "splash_svip_default_advertise_config", // 开屏超级会员兜底广告相关配置
-                  "splash_default_advertise_config", // 开屏普通用户兜底广告相关配置
-                  "splash_advertise_config" // 开屏广告相关配置
-                ].includes(i?.node_key)
-            );
-          for (let i of obj.splash_advertise_fetch_config_area.cfg_list) {
-            if (i?.switch) {
-              i.switch = "0";
-            }
-          }
-        }
-        if (obj?.splash_advertise_type_area?.cfg_list) {
-          for (let i of obj.splash_advertise_type_area.cfg_list) {
-            if (i?.switch) {
-              i.switch = "0";
-            }
-          }
-        }
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`百度网盘-开屏广告, 出现异常: ` + error);
-      }
-      break;
     // 嘀嗒出行-开屏广告
     case /^https:\/\/capis(-?\w*)?\.didapinche\.com\/ad\/cx\/startup\?/.test(
       url
