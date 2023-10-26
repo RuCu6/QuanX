10ada52c9407ac3248a65fb46722e45856ec29db
Cube <49463634+RuCu6@users.noreply.github.com>
2023-10-26 09:40:15 +0800

    taobao splash

--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,4 +1,4 @@
-// 2023-09-19 11:00
+// 2023-10-26 09:35
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -121,6 +121,84 @@ if (body) {
         body = JSON.stringify(obj);
       } catch (error) {
         console.log(`联享家-开屏广告, 出现异常: ` + error);
+      }
+      break;
+    // 淘宝-开屏视频广告
+    case /^https:\/\/guide-acs\.m\.taobao\.com\/gw\/mtop\.taobao\.cloudvideo\.video\.query/.test(url):
+      try {
+        let obj = JSON.parse(body);
+        if (obj?.data?.duration) {
+          obj.data.duration = "0";
+        }
+        if (obj?.data?.resources?.length > 0) {
+          obj.data.resources = [];
+        }
+        if (obj?.data?.caches?.length > 0) {
+          obj.data.caches = [];
+        }
+        if (obj?.data?.respTimeInMs) {
+          obj.data.respTimeInMs = "5364633600000";
+        }
+        body = JSON.stringify(obj);
+      } catch (error) {
+        console.log(`淘宝-开屏视频广告, 出现异常: ` + error);
+      }
+      break;
+    // 淘宝-开屏图片广告
+    case /^https:\/\/guide-acs\.m\.taobao\.com\/gw\/mtop\.taobao\.wireless\.home\.splash\.awesome\.get/.test(url):
+      try {
+        let obj = JSON.parse(body);
+        if (obj?.data?.containers?.splash_home_base) {
+          let splash = obj.data.containers.splash_home_base;
+          if (splash?.base?.sections?.length > 0) {
+            for (let items of splash.base.sections) {
+              if ("taobao-splash" in items.bizData) {
+                if (items["taobao-splash"]?.data?.length > 0) {
+                  for (let item of items.bizData["taobao-splash"].data) {
+                    item.times = "0";
+                    item.hotStart = "false";
+                    item.haveVoice = "false";
+                    item.hideTBLogo = "false";
+                    item.enable4G = "false";
+                    item.coldStart = "false";
+                    item.waitTime = "0";
+                    item.imgUrl = "";
+                    item.startTime = "5364633600000";
+                    item.endTime = "5364719999000";
+                    item.gmtStart = "2140-01-01 00:00:00";
+                    item.gmtEnd = "2140-01-01 23:59:59";
+                    item.gmtStartMs = "5364633600000";
+                    item.gmtEndMs = "5364719999000";
+                  }
+                }
+              }
+            }
+          }
+        }
+        body = JSON.stringify(obj);
+      } catch (error) {
+        console.log(`淘宝-开屏图片广告, 出现异常: ` + error);
+      }
+      break;
+    // 淘宝-开屏活动
+    case /^https:\/\/poplayer\.template\.alibaba\.com\/\w+\.json/.test(url):
+      try {
+        let obj = JSON.parse(body);
+        if (obj?.res?.images?.length > 0) {
+          obj.res.images = [];
+        }
+        if (obj?.res?.videos?.length > 0) {
+          obj.res.videos = [];
+        }
+        if (obj?.enable) {
+          obj.enable = false;
+        }
+        if (obj?.mainRes?.images?.length > 0) {
+          obj.mainRes.images = [];
+        }
+        body = JSON.stringify(obj);
+      } catch (error) {
+        console.log(`淘宝-开屏活动, 出现异常: ` + error);
       }
       break;
     // 小爱音箱-开屏广告
