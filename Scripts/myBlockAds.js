17c1e8e4d2da90e4b0cb7920299b95633d00f4d4
Cube <49463634+RuCu6@users.noreply.github.com>
2023-10-28 09:02:12 +0800

    jd mine

--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,4 +1,4 @@
-// 2023-10-26 09:35
+// 2023-10-28 09:00
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -85,6 +85,52 @@ if (body) {
         body = JSON.stringify(obj);
       } catch (error) {
         console.log(`JavDB, 出现异常: ` + error);
+      }
+      break;
+    // 京东-个人主页
+    case /^https:\/\/api\.m\.jd\.com\/client\.action\?functionId=personinfoBusiness/.test(url):
+      try {
+        let obj = JSON.parse(body);
+        if (obj?.floors?.length > 0) {
+          let newFloors = [];
+          for (let floor of obj.floors) {
+            const items = [
+              "bigSaleFloor",
+              "recommendfloor",
+              "newAttentionCard", // 关注的频道
+              "iconToolFloor",
+              "noticeFloor"
+            ];
+            if (items?.includes(floor?.mId)) {
+              continue;
+            } else {
+              if (floor?.mId === "basefloorinfo") {
+                // 弹窗
+                if (floor?.data?.commonPopup) {
+                  delete floor.data.commonPopup;
+                }
+                // 弹窗
+                if (floor?.data?.commonWindows?.length > 0) {
+                  floor.data.commonWindows = [];
+                }
+              } else if (floor?.mId === "userinfo") {
+                // 顶部背景图
+                if (floor?.data?.bgImgInfo?.bgImg) {
+                  delete floor.data.bgImgInfo.bgImg;
+                }
+                // 开通plus会员卡片
+                if (floor?.data?.newPlusBlackCard) {
+                  delete floor.data.newPlusBlackCard;
+                }
+              }
+              newFloors.push(floor);
+            }
+          }
+          obj.floors = newFloors;
+        }
+        body = JSON.stringify(obj);
+      } catch (error) {
+        console.log(`京东-个人主页, 出现异常: ` + error);
       }
       break;
     // 京东-开屏广告
