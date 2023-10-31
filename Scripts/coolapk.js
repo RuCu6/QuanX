cb595d0743461e3c4a1a7af67fc537bcc56f0ae6
Cube <49463634+RuCu6@users.noreply.github.com>
2023-10-31 19:24:32 +0800

    remove comment ad

--- Scripts/coolapk.js
+++ Scripts/coolapk.js
@@ -1,4 +1,4 @@
-// 2023-10-31 18:20
+// 2023-10-31 19:25
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -6,10 +6,10 @@ let obj = JSON.parse($response.body);
 
 if (url.includes("/feed/detail")) {
   if (obj.data?.hotReplyRows?.length > 0) {
-    obj.data.hotReplyRows = obj.data.hotReplyRows.filter((item) => item.id);
+    obj.data.hotReplyRows = obj.data.hotReplyRows.filter((item) => item?.id);
   }
   if (obj.data?.topReplyRows?.length > 0) {
-    obj.data.topReplyRows = obj.data.topReplyRows.filter((item) => item.id);
+    obj.data.topReplyRows = obj.data.topReplyRows.filter((item) => item?.id);
   }
   const item = ["detailSponsorCard", "include_goods", "include_goods_ids"];
   for (let i of item) {
@@ -19,24 +19,24 @@ if (url.includes("/feed/detail")) {
   }
 } else if (url.includes("/feed/replyList")) {
   if (obj.data?.length > 0) {
-    obj.data = obj.data.filter((item) => item.id);
+    obj.data = obj.data.filter((item) => item?.id);
   }
 } else if (url.includes("/main/dataList")) {
   if (obj.data?.length > 0) {
-    obj.data = obj.data.filter((item) => !(item.entityTemplate === "sponsorCard" || item.title === "精选配件"));
+    obj.data = obj.data.filter((item) => !(item?.entityTemplate === "sponsorCard" || item?.title === "精选配件"));
   }
 } else if (url.includes("/main/indexV8")) {
   if (obj.data?.length > 0) {
     obj.data = obj.data.filter(
       (item) =>
         !(
-          item.entityTemplate === "sponsorCard" ||
-          item.entityId === 8639 ||
-          item.entityId === 29349 ||
-          item.entityId === 33006 ||
-          item.entityId === 32557 ||
-          item.title.includes("值得买") ||
-          item.title.includes("红包")
+          item?.entityTemplate === "sponsorCard" ||
+          item?.entityId === 8639 ||
+          item?.entityId === 29349 ||
+          item?.entityId === 33006 ||
+          item?.entityId === 32557 ||
+          item?.title?.includes("值得买") ||
+          item?.title?.includes("红包")
         )
     );
   }
@@ -47,7 +47,10 @@ if (url.includes("/feed/detail")) {
   }
 } else if (url.includes("/page/dataList")) {
   if (obj.data?.length > 0) {
-    obj.data = obj.data.filter((item) => !(item.title === "酷安热搜"));
+    obj.data = obj.data.filter(
+      (item) =>
+        !(item?.title === "酷安热搜" || item?.entityTemplate === "imageScaleCard" || item?.entityTemplate === "sponsorCard")
+    );
   }
 }
 
