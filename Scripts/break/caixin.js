772782dd37792ba11264988a501b6daba6ab7780
Cube <49463634+RuCu6@users.noreply.github.com>
2023-12-10 10:55:26 +0800

    财新 调整脚本结构

--- Scripts/break/caixin.js
+++ Scripts/break/caixin.js
@@ -1,80 +1,11 @@
-// 2023-11-26 09:25
+// 2023-12-10 10:50
 
 var url = $request.url;
 var header = $request.headers;
 const isQuanX = typeof $task !== "undefined";
 const appinfo = "";
 
-if (url.includes("/api/dataplus/columns")) {
-  // 首页-数据通
-  let obj = JSON.parse($response.body);
-  const items = ["buttonColor", "buttonText", "buyUrl", "descText"];
-  if (obj?.data?.length > 0) {
-    for (let item of obj.data) {
-      for (let i of items) {
-        delete item[i];
-      }
-    }
-  }
-  $done({ body: JSON.stringify(obj) });
-} else if (url.includes("/caixinapp/appinfo")) {
-  // 文章详情页
-  let obj = JSON.parse($response.body);
-  if (obj?.data?.articlePromotionList?.length > 0) {
-    // 文章底部推广图
-    obj.data.articlePromotionList = [];
-  }
-  $done({ body: JSON.stringify(obj) });
-} else if (url.includes("/channelv5/list")) {
-  // 首页-顶部分类列表
-  let obj = JSON.parse($response.body);
-  delete obj.data.ios_ad_513;
-  delete obj.data.android_ad_513;
-  $done({ body: JSON.stringify(obj) });
-} else if (url.includes("/fm/index/list")) {
-  // 首页-财新fm
-  const items = ["androidAdList", "headlines", "iosAdList"];
-  if (obj?.data) {
-    for (let i of items) {
-      if (obj?.data?.[i]?.length > 0) {
-        obj.data[i] = [];
-      }
-    }
-  }
-} else if (url.includes("/gg.caixin.com/s")) {
-  // 开屏广告
-  let body = $response.body;
-  if (body === undefined) {
-    $done({});
-  } else {
-    const items = {
-      showfrequency: 0,
-      showintval: 0,
-      showcycle: 604800,
-      intval: 0,
-      sday: "2090-12-31 00:00:00",
-      eday: "2090-12-31 23:59:59"
-    };
-    body = body
-      .replace(/"showfrequency":\d+/g, items["showfrequency"])
-      .replace(/"showintval":\d+/g, items["showintval"])
-      .replace(/"showcycle":\d+/g, items["showcycle"])
-      .replace(/"intval":\d+/g, items["intval"])
-      .replace(/"sday":"[^"]*"/g, items["sday"])
-      .replace(/"eday":"[^"]*"/g, items["eday"])
-      .replace(/"banner":/g, "ban0:");
-    $done({ body });
-  }
-} else if (url.includes("/index_page_v5")) {
-  // 首页-信息流
-  let obj = JSON.parse($response.body);
-  delete obj.data.ios_ad_513;
-  delete obj.data.android_ad_513;
-  if (obj?.data?.list?.length > 0) {
-    obj.data.list = obj.data.list.filter((i) => !["金融我闻", "财新数据通"]?.includes(i?.channel_name));
-  }
-  $done({ body: JSON.stringify(obj) });
-} else if (url.includes("/validateAudioAuth") || url.includes("/groupImageValidate")) {
+if (url.includes("/validateAudioAuth") || url.includes("/groupImageValidate")) {
   // 会员数据
   header.appinfo = appinfo;
   delete header.authentication;
@@ -121,5 +52,68 @@ if (url.includes("/api/dataplus/columns")) {
   }
   $done({ url: url, headers: header });
 } else {
-  $done({});
+  if (url.includes("/gg.caixin.com/s")) {
+    // 开屏广告
+    let body = $response.body;
+    if (body === undefined) {
+      $done({});
+    } else {
+      body = body
+        .replace(/"showfrequency":\d+/g, '"showfrequency":0')
+        .replace(/"showintval":\d+/g, '"showintval":0')
+        .replace(/"showcycle":\d+/g, '"showcycle":604800')
+        .replace(/"intval":\d+/g, '"intval":0')
+        .replace(/"sday":"[^"]*"/g, '"sday":"2090-12-31 00:00:00"')
+        .replace(/"eday":"[^"]*"/g, '"eday":"2090-12-31 23:59:59"')
+        .replace(/"banner":/g, "ban0:");
+      $done({ body });
+    }
+  } else {
+    let obj = JSON.parse($response.body);
+    if (url.includes("/api/dataplus/columns")) {
+      // 首页-数据通
+      const items = ["buttonColor", "buttonText", "buyUrl", "descText"];
+      if (obj?.data?.length > 0) {
+        for (let item of obj.data) {
+          for (let i of items) {
+            delete item[i];
+          }
+        }
+      }
+    } else if (url.includes("/caixinapp/appinfo")) {
+      // 文章详情页
+      if (obj?.data?.articlePromotionList?.length > 0) {
+        // 文章底部推广图
+        obj.data.articlePromotionList = [];
+      }
+    } else if (url.includes("/channelv5/list")) {
+      // 首页-顶部分类列表
+      delete obj.data.ios_ad_513;
+      delete obj.data.android_ad_513;
+    } else if (url.includes("/fm/index/list")) {
+      // 首页-财新fm
+      const items = ["androidAdList", "headlines", "iosAdList"];
+      if (obj?.data) {
+        for (let i of items) {
+          if (obj?.data?.[i]?.length > 0) {
+            obj.data[i] = [];
+          }
+        }
+      }
+    } else if (url.includes("/index_page_v5")) {
+      // 首页-信息流
+      let obj = JSON.parse($response.body);
+      delete obj.data.ios_ad_513;
+      delete obj.data.android_ad_513;
+      if (obj?.data?.list?.length > 0) {
+        obj.data.list = obj.data.list.filter((i) => !["金融我闻", "财新数据通"]?.includes(i?.channel_name));
+      }
+    } else if (url.includes("/search/getkeyword")) {
+      // 搜索框填充词
+      if (obj?.data?.keys?.length > 0) {
+        obj.data.keys = [];
+      }
+    }
+    $done({ body: JSON.stringify(obj) });
+  }
 }
