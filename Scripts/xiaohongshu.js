bcbceeb802325f522719d9aa820e4dada9912eee
Cube <49463634+RuCu6@users.noreply.github.com>
2023-12-14 19:14:19 +0800

    Update xiaohongshu.js

--- Scripts/xiaohongshu.js
+++ Scripts/xiaohongshu.js
@@ -1,4 +1,4 @@
-// 2023-12-14 19:05
+// 2023-12-14 19:15
 
 const url = $request.url;
 const isQuanX = typeof $task !== "undefined";
@@ -91,7 +91,7 @@ if (url.includes("/v1/note/live_photo/save")) {
       $prefs.removeValueForKey("redBookLivePhoto");
       $prefs.setValueForKey(JSON.stringify(obj.data[0].note_list[0].images_list), "redBookLivePhoto");
     } else {
-      $persistentStore.write("", "redBookLivePhoto");
+      $persistentStore.write(null, "redBookLivePhoto");
       $persistentStore.write(JSON.stringify(obj.data[0].note_list[0].images_list), "redBookLivePhoto");
     }
   }
