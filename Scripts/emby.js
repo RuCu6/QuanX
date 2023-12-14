e377395d7636b477de533ed18904736f5c6810ee
Cube <49463634+RuCu6@users.noreply.github.com>
2023-12-14 08:50:45 +0800

    精简代码

--- Scripts/emby.js
+++ Scripts/emby.js
@@ -1,30 +1,19 @@
-// 2023-12-12 18:00
+// 2023-12-14 09:00
 
 const url = $request.url;
 const isQuanX = typeof $task != "undefined";
+const myStatus = isQuanX ? $response.statusCode : $response.status;
 const myBoby = '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}';
 
 if (url.includes("/registration/validateDevice")) {
-  if (isQuanX) {
-    if ($response.status !== "HTTP/1.1 200 OK") {
-      $done({
-        status: "HTTP/1.1 200 OK",
-        headers: $response.headers,
-        body: myBoby
-      });
-    } else {
-      $done({});
-    }
+  if (myStatus !== 200) {
+    $done({
+      status: "HTTP/1.1 200 OK",
+      headers: $response.headers,
+      body: myBoby
+    });
   } else {
-    if ($response.status !== 200) {
-      $done({
-        status: 200,
-        headers: $response.headers,
-        body: myBoby
-      });
-    } else {
-      $done({});
-    }
+    $done({});
   }
 } else {
   $done({});
