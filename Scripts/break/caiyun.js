f811b443d72fc45fa0f6fd463044ea7c36a2c9e7
Cube <49463634+RuCu6@users.noreply.github.com>
2023-12-14 22:28:28 +0800

    添加彩云会员数据

--- Scripts/break/caiyun.js
+++ Scripts/break/caiyun.js
@@ -1,37 +1,42 @@
-// 2023-07-11 08:35
+// 2023-12-14 22:25
 
 const url = $request.url;
-if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
+let header = $request.headers;
 
-if (url.includes("/v1/vip_info")) {
-  if (obj.vip) {
-    obj.vip.expires_time = "4030000000";
-  }
-  if (obj.svip) {
-    obj.svip.expires_time = "4030000000";
-  }
-} else if (url.includes("/v2/user")) {
-  if (obj.result) {
-    obj.result.svip_given = 730;
-    obj.result.is_phone_verified = true;
-    obj.result.is_xy_vip = true;
-    obj.result.vip_expired_at = 4030000000.16;
-    obj.result.is_vip = true;
-    obj.result.xy_svip_expire = 4030000000.16;
-    if (obj.result.wt) {
-      if (obj.result.wt.vip) {
-        obj.result.wt.vip.enabled = true;
-        obj.result.wt.vip.expired_at = 4030000000.16;
-        obj.result.wt.vip.svip_expired_at = 4030000000.16;
-      }
-      obj.result.wt.svip_given = 730;
+if (typeof $response === "undefined") {
+  header["device-token"] =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
+  $done({ headers: header })
+} else {
+  let obj = JSON.parse($response.body);
+  if (url.includes("/v1/vip_info")) {
+    if (obj.vip) {
+      obj.vip.expires_time = "4030000000";
     }
-    obj.result.is_primary = true;
-    obj.result.xy_vip_expire = 4030000000.16;
-    obj.result.svip_expired_at = 4030000000.16;
-    obj.result.vip_type = "s";
+    if (obj.svip) {
+      obj.svip.expires_time = "4030000000";
+    }
+  } else if (url.includes("/v2/user")) {
+    if (obj.result) {
+      obj.result.svip_given = 730;
+      obj.result.is_phone_verified = true;
+      obj.result.is_xy_vip = true;
+      obj.result.vip_expired_at = 4030000000.16;
+      obj.result.is_vip = true;
+      obj.result.xy_svip_expire = 4030000000.16;
+      if (obj.result.wt) {
+        if (obj.result.wt.vip) {
+          obj.result.wt.vip.enabled = true;
+          obj.result.wt.vip.expired_at = 4030000000.16;
+          obj.result.wt.vip.svip_expired_at = 4030000000.16;
+        }
+        obj.result.wt.svip_given = 730;
+      }
+      obj.result.is_primary = true;
+      obj.result.xy_vip_expire = 4030000000.16;
+      obj.result.svip_expired_at = 4030000000.16;
+      obj.result.vip_type = "s";
+    }
   }
+  $done({ body: JSON.stringify(obj) });
 }
-
-$done({ body: JSON.stringify(obj) });
