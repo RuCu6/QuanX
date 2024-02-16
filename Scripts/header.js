3de9e384c874b25571757855b90ea94819d84b84
Cube <49463634+RuCu6@users.noreply.github.com>
2024-02-16 17:35:56 +0800

    12306

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,9 +1,10 @@
-// 2023-08-24 12:45
+// 2024-02-16 17:25
 
 const url = $request.url;
 const header = $request.headers;
+const opt12306 = header["Operation-Type"] || header["operation-type"];
+const ua = header["User-Agent"] || header["user-agent"];
 const isQuanX = typeof $task !== "undefined";
-let ua = header["User-Agent"] || header["user-agent"];
 
 if (url.includes("/amdc/mobileDispatch")) {
   if (
@@ -19,6 +20,35 @@ if (url.includes("/amdc/mobileDispatch")) {
     }
   } else {
     $done({});
+  }
+} else if (url.includes("/mobile.12306.cn/otsmobile/")) {
+  const list12306 = [
+    // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
+    // "com.cars.otsmobile.checkLoginStatus", // 登录信息
+    "com.cars.otsmobile.city",
+    "com.cars.otsmobile.initCountry",
+    "com.cars.otsmobile.initNewSysCache",
+    "com.cars.otsmobile.initProvince",
+    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
+    "com.cars.otsmobile.memberInfo.getMemberQa",
+    "com.cars.otsmobile.memberInfo.integrationHomeInit",
+    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
+    "com.cars.otsmobile.newHomePage.initData",
+    "com.cars.otsmobile.newHomePageBussData",
+    "com.cars.otsmobile.newHomePageRefresh"
+  ];
+  if (isQuanX) {
+    if (list12306?.includes(opt12306)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done({});
+    }
+  } else {
+    if (list12306?.includes(opt12306)) {
+      $done();
+    } else {
+      $done({});
+    }
   }
 } else {
   $done({});
