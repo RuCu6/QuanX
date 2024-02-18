1c4b9e1589052d2c121e151b07e88d630937b44a
Cube <49463634+RuCu6@users.noreply.github.com>
2024-02-18 16:06:16 +0800

    移除 菜鸟回收弹窗

--- Scripts/cainiao.js
+++ Scripts/cainiao.js
@@ -1,97 +1,30 @@
-// 2023-12-19 09:35
+// 2024-02-18 15:50
 
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("nbfriend.message.conversation.list")) {
-  if (obj.data.data) {
-    obj.data.data = obj.data.data.filter((i) => i?.conversationId?.includes("logistic_message"));
-  }
-} else if (url.includes("nbpresentation.pickup.empty.page.get")) {
-  // 取件页面
-  if (obj.data.result) {
-    let ggContent = obj.data.result.content;
-    if (ggContent.middle) {
-      ggContent.middle = ggContent.middle.filter(
-        (i) =>
-          ![
-            "guoguo_pickup_empty_page_relation_add", // 添加亲友
-            "guoguo_pickup_helper_feedback", // 反馈组件
-            "guoguo_pickup_helper_tip_view" // 取件小助手
-          ]?.includes(i.template.name)
-      );
-    }
-  }
-} else if (url.includes("nbpresentation.protocol.homepage.get")) {
-  // 首页
-  if (obj.data.result) {
-    let res = obj.data.result;
-    if (res.dataList) {
-      res.dataList = res.dataList.filter((i) => {
-        if (i?.type?.includes("kingkong")) {
-          if (i.bizData.items) {
-            for (let ii of i.bizData.items) {
-              ii.rightIcon = null;
-              ii.bubbleText = null;
-            }
-            return true;
-          }
-        } else if (i?.type?.includes("icons_scroll")) {
-          // 顶部图标
-          if (i.bizData.items) {
-            const item = [
-              "618cjhb", // 超级红包
-              "bgxq", // 包裹星球
-              "cncy", // 填字赚现金
-              "cngy", // 免费领水果
-              "cngreen", // 绿色家园
-              "cnhs", // 菜鸟回收
-              "gjjf", // 裹酱积分
-              "jkymd", // 集卡赢免单
-              "ljjq", // 领寄件券
-              "ttlhb", // 天天领红包
-              "xybg" // 幸运包裹
-            ];
-            i.bizData.items = i.bizData.items.filter((ii) => !item?.includes(ii.key));
-            for (let ii of i.bizData.items) {
-              ii.rightIcon = null;
-              ii.bubbleText = null;
-            }
-            return true;
-          }
-        } else if (i?.type?.includes("banner_area")) {
-          // 新人福利 幸运抽奖
-          return false;
-        } else if (i?.type?.includes("promotion")) {
-          // 促销活动
-          return false;
-        } else {
-          return true;
-        }
-      });
-    }
-  }
-} else if (url.includes("guoguo.nbnetflow.ads.show")) {
+if (url.includes("guoguo.nbnetflow.ads.show")) {
   // 我的页面
-  if (obj.data.result) {
+  if (obj?.data?.result?.length > 0) {
     // 29338 寄件会员
     // 29339 裹酱积分
     // 33927 绿色能量
+    // 36649 回收旧物
     obj.data.result = obj.data.result.filter(
       (i) =>
         !(
           i?.materialContentMapper?.adItemDetail ||
           (i?.materialContentMapper?.bgImg && i?.materialContentMapper?.advRecGmtModifiedTime) ||
           ["common_header_banner", "entertainment", "kuaishou_banner"]?.includes(i?.materialContentMapper?.group_id) ||
-          ["29338", "29339", "32103", "33927"]?.includes(i.id)
+          ["29338", "29339", "32103", "33927", "36649"]?.includes(i?.id)
         )
     );
   }
 } else if (url.includes("guoguo.nbnetflow.ads.mshow")) {
   // 首页
-  if (obj.data) {
-    const item = [
+  if (obj?.data) {
+    const items = [
       "10", // 物流详情页 底部横图
       "498", // 物流详情页 左上角
       "328", // 3位数为家乡版本
@@ -114,11 +47,82 @@ if (url.includes("nbfriend.message.conversation.list")) {
       "1525", // 幸运包裹
       "1638" // 为你精选了一些商品
     ];
-    for (let i of item) {
+    for (let i of items) {
       if (obj.data?.[i]) {
         delete obj.data[i];
       }
     }
+  }
+} else if (url.includes("nbfriend.message.conversation.list")) {
+  if (obj?.data?.data?.length > 0) {
+    obj.data.data = obj.data.data.filter((i) => i?.conversationId?.includes("logistic_message"));
+  }
+} else if (url.includes("nbpresentation.pickup.empty.page.get")) {
+  // 取件页面
+  if (obj?.data?.result) {
+    let ggContent = obj.data.result.content;
+    if (ggContent?.middle?.length > 0) {
+      ggContent.middle = ggContent.middle.filter(
+        (i) =>
+          ![
+            "guoguo_pickup_empty_page_relation_add", // 添加亲友
+            "guoguo_pickup_helper_feedback", // 反馈组件
+            "guoguo_pickup_helper_tip_view" // 取件小助手
+          ]?.includes(i?.template?.name)
+      );
+    }
+  }
+} else if (url.includes("nbpresentation.protocol.homepage.get")) {
+  // 首页
+  if (obj?.data?.result?.dataList?.length > 0) {
+    let newLists = [];
+    for (let item of obj.data.result.dataList) {
+      if (item?.type?.includes("kingkong")) {
+        if (item?.bizData?.items?.length > 0) {
+          for (let i of item.bizData.items) {
+            i.rightIcon = null;
+            i.bubbleText = null;
+          }
+        }
+      } else if (item?.type?.includes("icons_scroll")) {
+        // 顶部图标
+        if (item?.bizData?.items?.length > 0) {
+          let newBizs = [];
+          for (let i of item.bizData.items) {
+            const lists = [
+              "618cjhb", // 超级红包
+              "bgxq", // 包裹星球
+              "cncy", // 填字赚现金
+              "cngy", // 免费领水果
+              "cngreen", // 绿色家园
+              "cnhs", // 菜鸟回收
+              "gjjf", // 裹酱积分
+              "jkymd", // 集卡赢免单
+              "ljjq", // 领寄件券
+              "ttlhb", // 天天领红包
+              "xybg" // 幸运包裹
+            ];
+            if (lists?.includes(i?.key)) {
+              continue;
+            }
+            newBizs.push(i);
+          }
+          item.bizData.items = newBizs;
+          for (let i of item.bizData.items) {
+            i.rightIcon = null;
+            i.bubbleText = null;
+          }
+        }
+      } else if (item?.type?.includes("banner_area")) {
+        // 新人福利 幸运抽奖
+        continue;
+      } else if (item?.type?.includes("promotion")) {
+        // 促销活动
+        continue;
+      }
+      newLists.push(item);
+    }
+    obj.data.result.dataList = newLists;
   }
 }
 
