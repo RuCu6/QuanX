bc9a5d6c9bd6a762b69b6094f21efbe680cfeac7
Cube <49463634+RuCu6@users.noreply.github.com>
2024-02-20 17:15:56 +0800

    Update header.js

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,4 +1,4 @@
-// 2024-02-17 09:45
+// 2024-02-20 17:15
 
 const url = $request.url;
 const header = $request.headers;
@@ -25,17 +25,17 @@ if (url.includes("/amdc/mobileDispatch")) {
   const list12306 = [
     // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
     // "com.cars.otsmobile.checkLoginStatus", // 登录信息
-    "com.cars.otsmobile.city",
-    "com.cars.otsmobile.initCountry",
-    "com.cars.otsmobile.initNewSysCache",
-    "com.cars.otsmobile.initProvince",
+    // "com.cars.otsmobile.city",
+    // "com.cars.otsmobile.initCountry",
+    // "com.cars.otsmobile.initNewSysCache",
+    // "com.cars.otsmobile.initProvince",
     "com.cars.otsmobile.integration.activityBanner", // 活动横幅
     "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
     // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
     // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
-    "com.cars.otsmobile.newHomePage.initData",
-    "com.cars.otsmobile.newHomePageBussData",
-    "com.cars.otsmobile.newHomePageRefresh",
+    "com.cars.otsmobile.newHomePage.initData", // 热门资讯
+    "com.cars.otsmobile.newHomePageBussData", // 商品信息流
+    // "com.cars.otsmobile.newHomePageRefresh",
     "com.cars.otsmobile.travelPage.initData" // 出行服务
   ];
   if (isQuanX) {
