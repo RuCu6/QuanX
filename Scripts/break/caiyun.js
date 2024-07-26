b57b0e3fa12efb630a4be6c65c574bd1ec6a193a
Cube <49463634+RuCu6@users.noreply.github.com>
2024-07-26 22:17:05 +0800

    Add files via upload

--- Scripts/break/caiyun.js
+++ Scripts/break/caiyun.js
@@ -1,4 +1,4 @@
-// 2024-07-16 09:00
+// 2024-07-23 19:55
 
 const url = $request.url;
 let header = $request.headers;
@@ -12,21 +12,12 @@ if (typeof $response === "undefined") {
   if (url.includes("/v1/activity")) {
     // 彩云推广
     if (["&type_id=A03&"]?.includes(url)) {
-      // 彩云AI
+      // 天气助手 彩云ai
       if (obj?.interval) {
         obj.interval = 2592000; // 30天===2592000秒
       }
       if (obj?.activities?.length > 0) {
-        let newActs = [];
-        for (let item of obj.activities) {
-          if (item?.type === "tabbar" && item?.feature) {
-            item.feature = false;
-          } else {
-            continue;
-          }
-          newActs.push(item);
-        }
-        obj.activities = newActs;
+        obj.activities = [];
       }
     } else {
       // 其他请求
