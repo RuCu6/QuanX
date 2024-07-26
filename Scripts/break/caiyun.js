1997cdd7ab6c955701832e1aea32007a31d63620
Cube <49463634+RuCu6@users.noreply.github.com>
2024-07-26 22:46:19 +0800

    移除  彩云ai

--- Scripts/break/caiyun.js
+++ Scripts/break/caiyun.js
@@ -1,4 +1,4 @@
-// 2024-07-23 19:55
+// 2024-07-26 22:45
 
 const url = $request.url;
 let header = $request.headers;
@@ -9,7 +9,36 @@ if (typeof $response === "undefined") {
   $done({ headers: header });
 } else {
   let obj = JSON.parse($response.body);
-  if (url.includes("/v1/activity")) {
+  if (url.includes("/api.caiyunapp.com/v1/activity")) {
+    if (url.includes("&type_id=A03&")) {
+      // 底栏控制项目 主页图标 天气助手 彩云ai
+      if (obj?.interval) {
+        obj.interval = 2592000; // 30天===2592000秒
+      }
+      if (obj?.activities?.length > 0) {
+        for (let item of obj.activities) {
+          if (item?.name && item?.type && item?.feature) {
+            item.feature = false;
+          }
+        }
+      }
+    } else {
+      // 其他请求
+      obj = {
+        status: "ok",
+        interval: 2592000,
+        id: "1",
+        activities: [
+          {
+            items: [{ text: "", image_light: "", link: "", activity_name: "", id: "1", image_dark: "" }],
+            type: "activity_icon",
+            name: "",
+            carousel: "5000"
+          }
+        ]
+      };
+    }
+  } else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
     // 彩云推广
     if (["&type_id=A03&"]?.includes(url)) {
       // 天气助手 彩云ai
