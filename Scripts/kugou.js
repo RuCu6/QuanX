cbe54a8609b4abb9315d6c4aba48745000ee162d
Cube <49463634+RuCu6@users.noreply.github.com>
2024-08-06 17:21:10 +0800

    Add files via upload

--- Scripts/kugou.js
+++ Scripts/kugou.js
@@ -1,4 +1,4 @@
-// 2024-08-03 08:30
+// 2024-08-06 17:15
 
 const url = $request.url;
 if (!$response) $done({});
@@ -49,6 +49,18 @@ if (
   if (obj?.data?.config) {
     // 开屏广告拉取配置
     obj.data.config = {};
+  }
+  if (obj?.data?.front_ads?.length > 0) {
+    // 开屏广告-请求3
+    for (let item of obj.data.front_ads) {
+      item.duration = 0;
+      item.image = "";
+      item.miit_button_extra_info = {};
+      item.miit_button_text = "";
+      item.show_times = 0;
+      item.start_time = "2090-12-31 00:00:00";
+      item.end_time = "2090-12-31 23:59:59";
+    }
   }
 } else if (url.includes("/gateway.kugou.com/adp/ad/v1/mine_top_banner?")) {
   // 我的页面顶部横图
