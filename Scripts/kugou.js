cc04fddb95e65cabdcb3c8b49fce2f961efafb6d
Cube <49463634+RuCu6@users.noreply.github.com>
2024-08-07 22:51:02 +0800

    Update kugou.js

--- Scripts/kugou.js
+++ Scripts/kugou.js
@@ -1,14 +1,11 @@
-// 2024-08-06 19:15
+// 2024-08-07 22:45
 
 const url = $request.url;
 if (!$response) $done({});
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (
-  url.includes("/adserviceretry.kglink.cn/v2/mobile_window?") ||
-  url.includes("/adserviceretry.kglink.cn/v4/mobile_window?")
-) {
+if (url.includes("/adserviceretry.kglink.cn/v2/mobile_window") || url.includes("/adserviceretry.kglink.cn/v4/mobile_window")) {
   // 签到弹窗 看广告领会员弹窗
   if (obj?.data?.ads?.length > 0) {
     for (let item of obj.data.ads) {
@@ -45,7 +42,23 @@ if (
   }
   if (obj?.data?.config) {
     // 开屏广告拉取配置
-    obj.data.config = {};
+    obj.data.config.gdt_policy_config = {
+      preload_interval_front: 0,
+      young_preload: 0,
+      fetch_mode: 0,
+      use_guarantee: 0,
+      vip_preload: 0,
+      preload_interval_boot: 0
+    };
+    obj.data.config.limit_times = 0;
+    obj.data.config.front_request_interval = 31536000;
+    obj.data.config.boot_display_interval = 31536000;
+    obj.data.config.duration_config = {
+      boot_fetch_ms: 31536000,
+      front_fetch_ms: 31536000,
+      front_timeout_ms: 31536000,
+      boot_timeout_ms: 31536000
+    };
   }
   if (obj?.data?.front_ads?.length > 0) {
     // 开屏广告-请求3
@@ -58,6 +71,10 @@ if (
       item.start_time = "2090-12-31 00:00:00";
       item.end_time = "2090-12-31 23:59:59";
     }
+  }
+  if (obj?.data?.retry_ads?.length > 0) {
+    // 开屏广告-请求4
+    obj.data.retry_ads = [];
   }
 } else if (
   url.includes("/gateway.kugou.com/adp/ad/v1/mine_top_banner") ||
