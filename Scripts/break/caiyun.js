32624f6f7ff00d4cbf57a930604652e55700b855
Cube <49463634+RuCu6@users.noreply.github.com>
2024-09-01 09:41:00 +0800

    Update caiyun.js

--- Scripts/break/caiyun.js
+++ Scripts/break/caiyun.js
@@ -1,11 +1,20 @@
-// 2024-07-26 22:45
+// 2024-09-01 09:15
 
 const url = $request.url;
+const isQuanX = typeof $task !== "undefined";
 let header = $request.headers;
 
 if (typeof $response === "undefined") {
-  header["device-token"] =
+  const cyTK =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
+  header["device-token"] = cyTK;
+  if (compareVersions(header.version, "7.1.9") > 0) {
+    if (isQuanX) {
+      header["Authorization"] = "Bearer " + cyTK;
+    } else {
+      header["authorization"] = "Bearer " + cyTK;
+    }
+  }
   $done({ headers: header });
 } else {
   let obj = JSON.parse($response.body);
@@ -24,19 +33,14 @@ if (typeof $response === "undefined") {
       }
     } else {
       // 其他请求
-      obj = {
-        status: "ok",
-        interval: 2592000,
-        id: "1",
-        activities: [
-          {
-            items: [{ text: "", image_light: "", link: "", activity_name: "", id: "1", image_dark: "" }],
-            type: "activity_icon",
-            name: "",
-            carousel: "5000"
-          }
-        ]
-      };
+      obj = { status: "ok", activities: [{ items: [] }] };
+    }
+  } else if (url.includes("/api/v1/user_detail")) {
+    if (obj?.vip_info?.svip) {
+      if (obj?.vip_info?.svip) {
+        obj.vip_info.svip.is_auto_renewal = true;
+        obj.vip_info.svip.expires_time = "3742732800";
+      }
     }
   } else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
     // 彩云推广
@@ -50,19 +54,7 @@ if (typeof $response === "undefined") {
       }
     } else {
       // 其他请求
-      obj = {
-        status: "ok",
-        interval: 2592000,
-        id: "1",
-        activities: [
-          {
-            items: [{ text: "", image_light: "", link: "", activity_name: "", id: "1", image_dark: "" }],
-            type: "activity_icon",
-            name: "",
-            carousel: "5000"
-          }
-        ]
-      };
+      obj = { status: "ok", activities: [{ items: [] }] };
     }
   } else if (url.includes("/v1/vip_info")) {
     // 我的页面
@@ -74,14 +66,14 @@ if (typeof $response === "undefined") {
     }
   } else if (url.includes("/v2/user")) {
     // 我的页面
-    if (obj.result) {
+    if (obj?.result) {
       obj.result.svip_given = 730;
       obj.result.is_phone_verified = true;
       obj.result.is_xy_vip = true;
       obj.result.vip_expired_at = 4030000000.16;
       obj.result.is_vip = true;
       obj.result.xy_svip_expire = 4030000000.16;
-      if (obj.result.wt) {
+      if (obj?.result?.wt) {
         if (obj.result.wt.vip) {
           obj.result.wt.vip.enabled = true;
           obj.result.wt.vip.expired_at = 4030000000.16;
@@ -96,4 +88,16 @@ if (typeof $response === "undefined") {
     }
   }
   $done({ body: JSON.stringify(obj) });
+}
+
+function compareVersions(t, r) {
+  const e = t.split(".").map(Number),
+    n = r.split(".").map(Number);
+  for (let t = 0; t < Math.max(e.length, n.length); t++) {
+    const r = e[t] || 0,
+      i = n[t] || 0;
+    if (r > i) return 1;
+    if (r < i) return -1;
+  }
+  return 0;
 }
